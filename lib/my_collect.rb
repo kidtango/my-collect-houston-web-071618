e3526a7b469a08f444b03639cc6ef75781d3ef76
Scott Tang <scott.q.tang@gmail.com>
2018-07-01 23:24:54 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,28 @@
+def my_collect(array)
+    if block_given?
+        i = 0
+        new_array = Array.new
+    
+        while i < array.length
+            new_array << yield(array[i])
+            i = i + 1
+        end
+        new_array
+    else
+        puts "No argument was provided. Expecting 1"
+    end
+
+end
 
 
+collection = ['Ruby d', 'javascript f', 'python f', 'objective-c']
+
+
+
+my_collect(collection) do |lang|
+    lang.upcase
+end
+
+my_collect(collection) do |first_name|
+    puts first_name.split(" ").first
+ end
